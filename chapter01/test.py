81f9aee7032918f6be34adbfd7c256aab3e4218b
niracler <1026037967@qq.com>
2018-06-16 21:15:05 +0800

    chapter01 安装环境

--- chapter01/test.py
+++ chapter01/test.py
@@ -1,8 +1,11 @@
 import cv2 as cv
 
-src = cv.imread("266679.png")
+src = cv.imread("./img/266679.png") #读取图片
+
+# 新建一个窗口并展示
 cv.namedWindow("input image", cv.WINDOW_AUTOSIZE)
 cv.imshow("input image", src)
 cv.waitKey(0)
 cv.destroyAllWindows()
+
 print("hello")
